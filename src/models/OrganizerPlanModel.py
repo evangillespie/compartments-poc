ad41caecd821404dec3f5555816c68827ddc304a
Evan Gillespie <evan.gillespie1@gmail.com>
2016-06-06 10:54:55 -0600

    Added a more detailed sample plan

--- src/models/OrganizerPlanModel.py
+++ src/models/OrganizerPlanModel.py
@@ -15,20 +15,62 @@ class OrganizerPlanModel(object):
 		pass
 
 
+	"""
+	Return a sample plan for the proof of concept
+
+	The real plans won't exist until there is a way for the user to create them
+	Eventually, the plan model should get a lot more rubust than this dictionary
+	"""
 	@classmethod
 	def get_sample_plan(cls):
 		'''
-		Return a sample plan for the proof of concept
-
-		The real plans won't exist until there is a way for the user to create them
-		Eventually, the plan model should get a lot more rubust than this dictionary
+		_________________
+		|				|
+		|				|
+		|				|
+		|				|
+		|_______________|
+		|		|		|
+		|		|		|
+		|		|		|
+		|		|		|
+		|_______|_______|
 		'''
+
 		return {
-			'width': 120,
-			'length': 200,
-			'height': 15,
-			'thickness': 6,
+			'x_length': 120,
+			'y_length': 200,
+			'height': 20,
+			'thickness': 6,	# Total heigth, including the base
 			'compartments': [
-				# @TODO: how do i do this?
+				{
+					#  y means that interior dividers are parallel to the x axis. Useful for interior compartments
+					'orientation': 'y',
+					'length': 102,
+					'compartments': [
+						{
+							'orientation': 'x',
+							'length': 91,
+							'compartments': [
+								{
+									'orientation': None,
+									'length': 48,
+									'compartments': []
+								},
+								{
+									'orientation': None,
+									'length': 48,
+									'compartments': []
+								}
+							]
+						},
+						{
+							'orientation': None,
+							'length': 91,
+							'compartments': []
+						}
+					]
+
+				}
 			]
 		}
