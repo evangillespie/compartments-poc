09ec36820f5179498ffc49475d5911da3c313ee1
Evan Gillespie <evan.gillespie1@gmail.com>
2016-06-30 22:09:40 -0500

    Added a simpler sample plan that has only one divider

--- src/controllers/OrganizerPlanController.py
+++ src/controllers/OrganizerPlanController.py
@@ -25,3 +25,8 @@ class OrganizerPlanController(object):
 		The real plans won't exist until there is a way for the user to create them
 		'''
 		return OrganizerPlanModel.get_sample_plan()
+
+
+	@classmethod
+	def get_simple_sample_plan(cls):
+		return OrganizerPlanModel.get_simple_sample_plan()
--- src/models/OrganizerPlanModel.py
+++ src/models/OrganizerPlanModel.py
@@ -106,3 +106,56 @@ class OrganizerPlanModel(object):
 				}
 			]
 		}
+
+
+	"""
+	Return a simpler version of the sample plan for debugging
+	"""
+	@classmethod
+	def get_simple_sample_plan(cls):
+		'''
+		_________________
+		|				|
+		|				|
+		|				|
+		|				|
+		|_______________|
+		|				|
+		|		 		|
+		|		 		|
+		|		 		|
+		|_______________|
+		'''
+
+		return {
+			'x_length': 120,
+			'y_length': 200,
+			'height': 50,
+			'thickness': 6,	# Total heigth, including the base
+			'compartments': [
+				{
+					'level': 1,	# useful for debugging
+					#  y means that interior dividers are parallel to the y axis.
+					'div_orientation': 'x',
+					'x_length': 108,
+					'y_length': 188,
+					'compartments': [
+						{
+							'level': 2,
+							'div_orientation': 'y',
+							'x_length': 108,
+							'y_length': 91,
+							'compartments': []
+						},
+						{
+							'level': 2,
+							'div_orientation': None,
+							'x_length': 108,
+							'y_length': 91,
+							'compartments': []
+						}
+					]
+
+				}
+			]
+		}
